3a79a2067e269669e3971fd8adef89abea985654
Jeff Epler <jepler@gmail.com>
2025-02-19 14:23:13 -0600

    Add terminal-like behavior. Reformat with clang-format

--- src/Adafruit_dvhstx.cpp
+++ src/Adafruit_dvhstx.cpp
@@ -1,44 +1,108 @@
 #include "Adafruit_dvhstx.h"
 
 int16_t dvhstx_width(DVHSTXResolution r) {
-    switch(r) {
-default:
-        case DVHSTX_RESOLUTION_320x180: return 320;
-        case DVHSTX_RESOLUTION_640x360: return 640;
-        case DVHSTX_RESOLUTION_480x270: return 480;
-        case DVHSTX_RESOLUTION_400x225: return 400;
-        case DVHSTX_RESOLUTION_320x240: return 320;
-        case DVHSTX_RESOLUTION_360x240: return 360;
-        case DVHSTX_RESOLUTION_360x200: return 360;
-        case DVHSTX_RESOLUTION_360x288: return 360;
-        case DVHSTX_RESOLUTION_400x300: return 400;
-        case DVHSTX_RESOLUTION_512x384: return 512;
-        case DVHSTX_RESOLUTION_400x240: return 400;
-    }
-    return 0;
+  switch (r) {
+  default:
+  case DVHSTX_RESOLUTION_320x180:
+    return 320;
+  case DVHSTX_RESOLUTION_640x360:
+    return 640;
+  case DVHSTX_RESOLUTION_480x270:
+    return 480;
+  case DVHSTX_RESOLUTION_400x225:
+    return 400;
+  case DVHSTX_RESOLUTION_320x240:
+    return 320;
+  case DVHSTX_RESOLUTION_360x240:
+    return 360;
+  case DVHSTX_RESOLUTION_360x200:
+    return 360;
+  case DVHSTX_RESOLUTION_360x288:
+    return 360;
+  case DVHSTX_RESOLUTION_400x300:
+    return 400;
+  case DVHSTX_RESOLUTION_512x384:
+    return 512;
+  case DVHSTX_RESOLUTION_400x240:
+    return 400;
+  }
+  return 0;
 }
 
 int16_t dvhstx_height(DVHSTXResolution r) {
-    switch(r) {
-default:
-        case DVHSTX_RESOLUTION_320x180: return 180;
-        case DVHSTX_RESOLUTION_640x360: return 360;
-        case DVHSTX_RESOLUTION_480x270: return 270;
-        case DVHSTX_RESOLUTION_400x225: return 225;
-        case DVHSTX_RESOLUTION_320x240: return 240;
-        case DVHSTX_RESOLUTION_360x240: return 240;
-        case DVHSTX_RESOLUTION_360x200: return 200;
-        case DVHSTX_RESOLUTION_360x288: return 288;
-        case DVHSTX_RESOLUTION_400x300: return 300;
-        case DVHSTX_RESOLUTION_512x384: return 384;
-        case DVHSTX_RESOLUTION_400x240: return 240;
-    }
+  switch (r) {
+  default:
+  case DVHSTX_RESOLUTION_320x180:
+    return 180;
+  case DVHSTX_RESOLUTION_640x360:
+    return 360;
+  case DVHSTX_RESOLUTION_480x270:
+    return 270;
+  case DVHSTX_RESOLUTION_400x225:
+    return 225;
+  case DVHSTX_RESOLUTION_320x240:
+    return 240;
+  case DVHSTX_RESOLUTION_360x240:
+    return 240;
+  case DVHSTX_RESOLUTION_360x200:
+    return 200;
+  case DVHSTX_RESOLUTION_360x288:
+    return 288;
+  case DVHSTX_RESOLUTION_400x300:
+    return 300;
+  case DVHSTX_RESOLUTION_512x384:
+    return 384;
+  case DVHSTX_RESOLUTION_400x240:
+    return 240;
+  }
 }
 
 void DVHSTX16::swap(bool copy_framebuffer) {
-    if (!double_buffered) { return; }
-    hstx.flip_blocking();
-    if (copy_framebuffer) {
-        memcpy(hstx.get_front_buffer<uint8_t>(), hstx.get_back_buffer<uint8_t>(), sizeof(uint16_t) * _width * _height);
-    } 
+  if (!double_buffered) {
+    return;
+  }
+  hstx.flip_blocking();
+  if (copy_framebuffer) {
+    memcpy(hstx.get_front_buffer<uint8_t>(), hstx.get_back_buffer<uint8_t>(),
+           sizeof(uint16_t) * _width * _height);
+  }
+}
+void DVHSTX8::swap(bool copy_framebuffer) {
+  if (!double_buffered) {
+    return;
+  }
+  hstx.flip_blocking();
+  if (copy_framebuffer) {
+    memcpy(hstx.get_front_buffer<uint8_t>(), hstx.get_back_buffer<uint8_t>(),
+           sizeof(uint8_t) * _width * _height);
+  }
+}
+
+void DVHSTXText3::clear() {
+  memset(getBuffer(), 0, WIDTH * HEIGHT * sizeof(uint16_t));
+}
+
+// Character framebuffer is actually a small GFXcanvas16, so...
+size_t DVHSTXText3::write(uint8_t c) {
+  if (c == '\r') { // Carriage return
+    cursor_x = 0;
+  } else if ((c == '\n') ||
+             (c >= 32 &&
+              cursor_x > WIDTH)) { // Newline OR right edge and printing
+    cursor_x = 0;
+    if (cursor_y >= (HEIGHT - 1)) { // Vert scroll?
+      memmove(getBuffer(), getBuffer() + WIDTH,
+              WIDTH * (HEIGHT - 1) * sizeof(uint16_t));
+      drawFastHLine(0, HEIGHT - 1, WIDTH, ' '); // Clear bottom line
+      cursor_y = HEIGHT - 1;
+    } else {
+      cursor_y++;
+    }
+  }
+  if (c >= 32) {
+    drawPixel(cursor_x, cursor_y, (attr << 8) | c);
+    cursor_x++;
+  }
+  sync_cursor_with_hstx();
+  return 1;
 }
--- src/Adafruit_dvhstx.h
+++ src/Adafruit_dvhstx.h
@@ -5,26 +5,29 @@
 #include "drivers/dvhstx/dvhstx.hpp"
 
 enum DVHSTXResolution {
-    /* well supported, square pixels on a 16:9 display, actual resolution 1280x720@50Hz */
-    DVHSTX_RESOLUTION_320x180,
-    DVHSTX_RESOLUTION_640x360,
+  /* well supported, square pixels on a 16:9 display, actual resolution
+     1280x720@50Hz */
+  DVHSTX_RESOLUTION_320x180,
+  DVHSTX_RESOLUTION_640x360,
 
-    /* sometimes supported, square pixels on a 16:9 display, actual resolution 960x540@60Hz */
-    DVHSTX_RESOLUTION_480x270,
+  /* sometimes supported, square pixels on a 16:9 display, actual resolution
+     960x540@60Hz */
+  DVHSTX_RESOLUTION_480x270,
 
-    /* sometimes supported, square pixels on a 16:9 display, actual resolution 800x450@60Hz */
-    DVHSTX_RESOLUTION_400x225,
+  /* sometimes supported, square pixels on a 16:9 display, actual resolution
+     800x450@60Hz */
+  DVHSTX_RESOLUTION_400x225,
 
-    /* well supported, but pixels aren't square on a 16:9 display */
-    DVHSTX_RESOLUTION_320x240, /* 4:3, actual resolution 640x480@60Hz */
-    DVHSTX_RESOLUTION_360x240, /* 3:2, actual resolution 720x480@60Hz */
-    DVHSTX_RESOLUTION_360x200, /* 18:10, actual resolution 720x400@70Hz */
-    DVHSTX_RESOLUTION_360x288, /* 5:4, actual resolution 720x576@60Hz */
-    DVHSTX_RESOLUTION_400x300, /* 4:3, actual resolution 800x600@60Hz */
-    DVHSTX_RESOLUTION_512x384, /* 4:3, actual resolution 1024x768@60Hz */
+  /* well supported, but pixels aren't square on a 16:9 display */
+  DVHSTX_RESOLUTION_320x240, /* 4:3, actual resolution 640x480@60Hz */
+  DVHSTX_RESOLUTION_360x240, /* 3:2, actual resolution 720x480@60Hz */
+  DVHSTX_RESOLUTION_360x200, /* 18:10, actual resolution 720x400@70Hz */
+  DVHSTX_RESOLUTION_360x288, /* 5:4, actual resolution 720x576@60Hz */
+  DVHSTX_RESOLUTION_400x300, /* 4:3, actual resolution 800x600@60Hz */
+  DVHSTX_RESOLUTION_512x384, /* 4:3, actual resolution 1024x768@60Hz */
 
-    /* sometimes supported, but pixels aren't square on a 16:9 display */
-    DVHSTX_RESOLUTION_400x240, /* 5:3, actual resolution 800x480@60Hz */
+  /* sometimes supported, but pixels aren't square on a 16:9 display */
+  DVHSTX_RESOLUTION_400x240, /* 5:3, actual resolution 800x480@60Hz */
 };
 
 using pimoroni::DVHSTXPinout;
@@ -37,7 +40,8 @@ using pimoroni::DVHSTXPinout;
 // positive ("P") pins in the order: Clock, Data 0, Data 1, Data 2; check your
 // board's schematic for details.
 #if defined(PIN_CKP)
-#define DVHSTX_PINOUT_DEFAULT {PIN_CKP, PIN_D0P, PIN_D1P, PIN_D2P}
+#define DVHSTX_PINOUT_DEFAULT                                                  \
+  { PIN_CKP, PIN_D0P, PIN_D1P, PIN_D2P }
 #endif
 
 int16_t dvhstx_width(DVHSTXResolution r);
@@ -45,29 +49,37 @@ int16_t dvhstx_height(DVHSTXResolution r);
 
 class DVHSTX16 : public GFXcanvas16 {
 public:
-    /**************************************************************************/
-    /*!
-       @brief    Instatiate a DVHSTX 16-bit canvas context for graphics
-       @param    res   Display resolution
-       @param    double_buffered Whether to allocate two buffers
-    */
-    /**************************************************************************/
-    DVHSTX16(DVHSTXPinout pinout, DVHSTXResolution res, bool double_buffered=false) : GFXcanvas16(dvhstx_width(res), dvhstx_height(res), false), pinout(pinout), res{res}, double_buffered{double_buffered} {}
-    ~DVHSTX16() { end(); }
+  /**************************************************************************/
+  /*!
+     @brief    Instatiate a DVHSTX 16-bit canvas context for graphics
+     @param    res   Display resolution
+     @param    double_buffered Whether to allocate two buffers
+  */
+  /**************************************************************************/
+  DVHSTX16(DVHSTXPinout pinout, DVHSTXResolution res,
+           bool double_buffered = false)
+      : GFXcanvas16(dvhstx_width(res), dvhstx_height(res), false),
+        pinout(pinout), res{res}, double_buffered{double_buffered} {}
+  ~DVHSTX16() { end(); }
 
-    bool begin() {
-        bool result = hstx.init(dvhstx_width(res), dvhstx_height(res), pimoroni::DVHSTX::MODE_RGB565, double_buffered, pinout);
-        if (!result) return false;
-        buffer = hstx.get_back_buffer<uint16_t>();
-        fillScreen(0);
-        return true;
-    }
-    void end() { hstx.reset(); }
+  bool begin() {
+    bool result =
+        hstx.init(dvhstx_width(res), dvhstx_height(res),
+                  pimoroni::DVHSTX::MODE_RGB565, double_buffered, pinout);
+    if (!result)
+      return false;
+    buffer = hstx.get_back_buffer<uint16_t>();
+    fillScreen(0);
+    return true;
+  }
+  void end() { hstx.reset(); }
 
   /**********************************************************************/
   /*!
-    @brief    If double-buffered, wait for retrace and swap buffers. Otherwise, do nothing (returns immediately)
-    @param copy_framebuffer if true, copy the new screen to the new back buffer. Otherwise, the content is undefined.
+    @brief    If double-buffered, wait for retrace and swap buffers. Otherwise,
+    do nothing (returns immediately)
+    @param copy_framebuffer if true, copy the new screen to the new back buffer.
+    Otherwise, the content is undefined.
   */
   /**********************************************************************/
   void swap(bool copy_framebuffer = false);
@@ -86,108 +98,140 @@ public:
   }
 
 private:
-DVHSTXPinout pinout;
-DVHSTXResolution res;
-    mutable pimoroni::DVHSTX hstx;
-bool double_buffered;
+  DVHSTXPinout pinout;
+  DVHSTXResolution res;
+  mutable pimoroni::DVHSTX hstx;
+  bool double_buffered;
 };
-
 
 class DVHSTX8 : public GFXcanvas8 {
 public:
-    /**************************************************************************/
-    /*!
-       @brief    Instatiate a DVHSTX 8-bit canvas context for graphics
-       @param    res   Display resolution
-       @param    double_buffered Whether to allocate two buffers
-    */
-    /**************************************************************************/
-    DVHSTX8(DVHSTXPinout pinout, DVHSTXResolution res, bool double_buffered=false) : GFXcanvas8(dvhstx_width(res), dvhstx_height(res), false), pinout(pinout), res{res}, double_buffered{double_buffered} {}
-    ~DVHSTX8() { end(); }
+  /**************************************************************************/
+  /*!
+     @brief    Instatiate a DVHSTX 8-bit canvas context for graphics
+     @param    res   Display resolution
+     @param    double_buffered Whether to allocate two buffers
+  */
+  /**************************************************************************/
+  DVHSTX8(DVHSTXPinout pinout, DVHSTXResolution res,
+          bool double_buffered = false)
+      : GFXcanvas8(dvhstx_width(res), dvhstx_height(res), false),
+        pinout(pinout), res{res}, double_buffered{double_buffered} {}
+  ~DVHSTX8() { end(); }
 
-    bool begin() {
-        bool result = hstx.init(dvhstx_width(res), dvhstx_height(res), pimoroni::DVHSTX::MODE_PALETTE, double_buffered, pinout);
-        if (!result) return false;
-        for(int i=0; i<255; i++ ) {
-            uint8_t r = (i >> 6) * 255 / 3;
-            uint8_t g = ((i >> 2) & 7) * 255 / 7;
-            uint8_t b = (i & 3) * 255 / 3;
-            setColor(i, r, g, b);
-        }
-        buffer = hstx.get_back_buffer<uint8_t>();
-        fillScreen(0);
-        return true;
+  bool begin() {
+    bool result =
+        hstx.init(dvhstx_width(res), dvhstx_height(res),
+                  pimoroni::DVHSTX::MODE_PALETTE, double_buffered, pinout);
+    if (!result)
+      return false;
+    for (int i = 0; i < 255; i++) {
+      uint8_t r = (i >> 6) * 255 / 3;
+      uint8_t g = ((i >> 2) & 7) * 255 / 7;
+      uint8_t b = (i & 3) * 255 / 3;
+      setColor(i, r, g, b);
     }
-    void end() { hstx.reset(); }
+    buffer = hstx.get_back_buffer<uint8_t>();
+    fillScreen(0);
+    return true;
+  }
+  void end() { hstx.reset(); }
 
-    void setColor(uint8_t idx, uint8_t red, uint8_t green, uint8_t blue) {
-        hstx.get_palette()[idx] = (red << 16) | (green << 8) | blue;
-    }
-    void setColor(uint8_t idx, uint32_t rgb) {
-        hstx.get_palette()[idx] = rgb;
-    }
+  void setColor(uint8_t idx, uint8_t red, uint8_t green, uint8_t blue) {
+    hstx.get_palette()[idx] = (red << 16) | (green << 8) | blue;
+  }
+  void setColor(uint8_t idx, uint32_t rgb) { hstx.get_palette()[idx] = rgb; }
 
   /**********************************************************************/
   /*!
-    @brief    If double-buffered, wait for retrace and swap buffers. Otherwise, do nothing (returns immediately)
-    @param copy_framebuffer if true, copy the new screen to the new back buffer. Otherwise, the content is undefined.
+    @brief    If double-buffered, wait for retrace and swap buffers. Otherwise,
+    do nothing (returns immediately)
+    @param copy_framebuffer if true, copy the new screen to the new back buffer.
+    Otherwise, the content is undefined.
   */
   /**********************************************************************/
   void swap(bool copy_framebuffer = false);
 
 private:
-DVHSTXPinout pinout;
-DVHSTXResolution res;
-    mutable pimoroni::DVHSTX hstx;
-bool double_buffered;
+  DVHSTXPinout pinout;
+  DVHSTXResolution res;
+  mutable pimoroni::DVHSTX hstx;
+  bool double_buffered;
 };
 
 using TextColor = pimoroni::DVHSTX::TextColour;
 
 class DVHSTXText3 : public GFXcanvas16 {
 public:
-    struct Cell {
-        uint16_t value;
-        Cell(uint8_t c, uint8_t attr = TextColor::TEXT_WHITE) : value(c | (attr << 8)) {}
-    };
-    /**************************************************************************/
-    /*!
-       @brief    Instatiate a DVHSTX 8-bit canvas context for graphics
-       @param    res   Display resolution
-       @param    double_buffered Whether to allocate two buffers
-    */
-    /**************************************************************************/
-    DVHSTXText3(DVHSTXPinout pinout) : GFXcanvas16(91,30,false), pinout(pinout), res{res}, attr{TextColor::TEXT_WHITE} {}
-    ~DVHSTXText3() { end(); }
+  struct Cell {
+    uint16_t value;
+    Cell(uint8_t c, uint8_t attr = TextColor::TEXT_WHITE)
+        : value(c | (attr << 8)) {}
+  };
+  /**************************************************************************/
+  /*!
+     @brief    Instatiate a DVHSTX 8-bit canvas context for graphics
+     @param    res   Display resolution
+     @param    double_buffered Whether to allocate two buffers
+  */
+  /**************************************************************************/
+  DVHSTXText3(DVHSTXPinout pinout)
+      : GFXcanvas16(91, 30, false),
+        pinout(pinout), res{res}, attr{TextColor::TEXT_WHITE} {}
+  ~DVHSTXText3() { end(); }
 
-    bool begin() {
-        bool result = hstx.init(91, 30, pimoroni::DVHSTX::MODE_TEXT_RGB111, false, pinout);
-        if (!result) return false;
-        buffer = hstx.get_back_buffer<uint16_t>();
-        return true;
-    }
-    void end() { hstx.reset(); }
+  bool begin() {
+    bool result =
+        hstx.init(91, 30, pimoroni::DVHSTX::MODE_TEXT_RGB111, false, pinout);
+    if (!result)
+      return false;
+    buffer = hstx.get_back_buffer<uint16_t>();
+    return true;
+  }
+  void end() { hstx.reset(); }
 
-    void cursor_off() { hstx.cursor_off(); }
-    void set_cursor(int x, int y) { hstx.set_cursor(x, y); }
+  void clear();
 
-#if 0 // TODO
-    void setattr(uint8_t a) { attr = a; };
-    uint8_t getattr(void) { return attr; }
+  void set_color(TextColor a) { attr = a; }
 
-    void scrollup(int rows=1);
-    void scrollregion(uint8_t xsrc, uint8_t ysrc, uint8_t xdest, uint8_t ydest, uint8_t cols, uint8_t rows);
+  void hide_cursor() {
+    cursor_visible = false;
+    hstx.cursor_off();
+  }
 
-    Cell getchar(uint8_t x, uint8_t y);
-    void setchar(uint8_t x, uint8_t y, Cell c);
-    void setchar(uint8_t x, uint8_t y, uint8_t ch) { setchar(x, y, {ch, attr}); }
-    void setchar(uint8_t x, uint8_t y, uint8_t ch, uint8_t a) { setchar(x, y, {ch, a}); }
-#endif
+  void show_cursor() {
+    cursor_visible = true;
+    sync_cursor_with_hstx();
+  }
+
+  void set_cursor(int x, int y) {
+    if (x < 0)
+      x = 0;
+    if (x > _width)
+      x = _width;
+    if (y < 0)
+      y = 0;
+    if (y >= _height)
+      y = _height - 1;
+    cursor_x = x;
+    cursor_y = y;
+    sync_cursor_with_hstx();
+  }
+
+  size_t write(uint8_t c);
 
 private:
-DVHSTXPinout pinout;
-DVHSTXResolution res;
-    mutable pimoroni::DVHSTX hstx;
-bool double_buffered;
-uint8_t attr;
+  DVHSTXPinout pinout;
+  DVHSTXResolution res;
+  mutable pimoroni::DVHSTX hstx;
+  bool double_buffered;
+  bool cursor_visible = false;
+  TextColor attr;
+  uint8_t cursor_x = 0, cursor_y = 0;
+
+  void sync_cursor_with_hstx() {
+    if (cursor_visible) {
+      hstx.set_cursor(cursor_x == _width ? _width - 1 : cursor_x, cursor_y);
+    }
+  }
 };
